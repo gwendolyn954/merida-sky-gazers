05981bc8b74ceb3b4b68e798ab42e74186e3a017
Gwendolyn Asani <107776357+gwendolyn954@users.noreply.github.com>
2023-04-09 21:26:29 -0600

    added initial files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.querySelector('button').addEventListener('click', viewAPOD)
+
+document.querySelector('button').addEventListener('click', displayImage)
+
+document.querySelector('button').addEventListener('click', displayOverview)
+
+function viewAPOD(){
+    const choice = document.querySelector('input').value
+    console.log(choice)
+    const url = `https://api.nasa.gov/planetary/apod?api_key=Avn0dhFfUv4hPGRJ9Fal8vFTxsMzcbKxHbQDu9Jx&date=${choice}`
+
+    fetch(url)
+      .then(res => res.json()) 
+      .then(data => {
+        console.log(data)
+        if(data.media_type === "image"){
+          document.querySelector('img').src = data.hdurl
+        }else{
+          alert('APOD not availablle for this date..')
+        }
+       
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+function displayOverview(){
+    let overview = document.getElementById('overview')
+    overview.style.display = "block";
+}
+
+function displayImage(){
+    let overview = document.getElementById('img')
+    overview.style.display = "block";
+}
